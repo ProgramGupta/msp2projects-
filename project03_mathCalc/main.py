5d2c6f100f100f0fb4c038f55500818f938966d5
Nikhil Gupta <110983734+ProgramGupta@users.noreply.github.com>
2022-09-08 14:34:32 +0000

    Finished 3 projects

--- project03_mathCalc/main.py
+++ project03_mathCalc/main.py
@@ -1,7 +1,8 @@
+import math
 #Pick the Equation
 what_equation_to_pick = int(
     input(
-        f'What equation do you want to solve for: 1 = Newton second law, 2 = Pythagorean theorem :'
+        f'What equation do you want to solve for: 1 = Newton second law, 2 = Pythagorean theorem 3 = area of a square:'
     ))
 
 #Newtons Second Law
@@ -36,20 +37,46 @@ elif what_equation_to_pick == 2:
         possible_variable_solving_for = input(
             f' What would you like to solve for?, a, b, or c : ')
         if possible_variable_solving_for == "a":
-            b = int(input('What is a equal to:'))
+            b = int(input('What is b equal to:'))
             c = int(input('What is c equal to:'))
-            a = ((c * c) / (b * b)) / 2
-            print(f'f = {b}')
+            a = math.sqrt(int((c**2) - (b**2)))
+            print(f'a = {a}')
             test_variable = False
         elif possible_variable_solving_for == "b":
             a = int(input('What is a equal to:'))
             c = int(input('What is c equal to:'))
-            b = ((c * c) / (a * a)) / 2
-            print(f'a = {a}')
+            b = math.sqrt(int((c**2) - (a**2)))
+            math.sqr(int(b))
+            print(f'b = {b}')
             test_variable = False
         elif possible_variable_solving_for == "c":
             a = int(input('What is a equal to:'))
             b = int(input('What is b equal to:'))
-            c = ((a * a) * (b * b)) / 2
-            print(f'm = {c}')
+            c = math.sqrt(int((a**2) + (b**2)))
+
+            print(f'c = {c}')
+            test_variable = False
+# Area of a Square
+elif what_equation_to_pick == 3:
+    test_variable = True
+    while test_variable == True:
+        possible_variable_solving_for = input(
+            f' What would you like to solve for?, b, h, or a : ')
+        if possible_variable_solving_for == "b":
+            a = int(input('What is a equal to:'))
+            h = int(input('What is h equal to:'))
+            b = a / h
+            print(f'b = {b}')
+            test_variable = False
+        elif possible_variable_solving_for == "h":
+            b = int(input('What is b equal to:'))
+            a = int(input('What is a equal to:'))
+            h = a / b
+            print(f'h = {h}')
+            test_variable = False
+        elif possible_variable_solving_for == "a":
+            b = int(input('What is b equal to:'))
+            h = int(input('What is h equal to:'))
+            a = b * h
+            print(f'a = {a}')
             test_variable = False
